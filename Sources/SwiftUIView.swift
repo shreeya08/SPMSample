36edea56c2641ac44c4ae1d28d049db5759be795
Shreeya Joshi <shreeya.joshi08@gmail.com>
2022-09-06 13:47:42 +0530

    initial Commit

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shreeya on 06/09/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct swiftUIView : View {
+    var body ":some View {
+        Text("Sample view from the package ")
+    Image(uiImage : UIImage(named: "Image", in: .module, with: nil)
+        .resizable()
+        .frame(width: 100, height: 100, alignment: .center)
+        .aspectRatio(contentMode: .fit)
+    }
+}
