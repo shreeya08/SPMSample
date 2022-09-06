842ef30705b4662970286dfeb248e511eaff23f4
Shreeya Joshi <shreeya.joshi08@gmail.com>
2022-09-06 15:48:54 +0530

    commit

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -13,10 +13,10 @@ public struct swiftUIView : View {
     public init() {}
     var body : some View {
         Text("Sample view from the package")
-//        Image(uiImage : UIImage(named: "Image", in: .module, with: nil)
-//        .resizable()
-//        .frame(width: 100, height: 100, alignment: .center)
-//        .aspectRatio(contentMode: .fit)
+        Image(uiImage : UIImage(named: "Image", in: .module, with: nil)
+        .resizable()
+        .frame(width: 100, height: 100, alignment: .center)
+        .aspectRatio(contentMode: .fit)
     }
           //Test
 }
